5b2938903d13142be14c6b9a150b378ef0fbc948
Binukalewke <binukalewke8@gmail.com>
2025-04-28 12:04:07 +0530

    added local storage save the Username

--- app/src/main/java/com/example/movienew/storage/LocalStorage.kt
+++ app/src/main/java/com/example/movienew/storage/LocalStorage.kt
@@ -7,12 +7,14 @@ object LocalStorage {
     private const val PREF_NAME = "user_credentials"
     private const val KEY_EMAIL = "email"
     private const val KEY_PASSWORD = "password"
+    private const val KEY_USERNAME = "username"
 
-    fun saveCredentials(context: Context, email: String, password: String) {
+    fun saveCredentials(context: Context, email: String, password: String, username: String) {
         val prefs: SharedPreferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
         prefs.edit().apply {
             putString(KEY_EMAIL, email)
             putString(KEY_PASSWORD, password)
+            putString(KEY_USERNAME, username)
             apply()
         }
     }
@@ -31,6 +33,11 @@ object LocalStorage {
         val email = LocalStorage.getEmail(context)
         val password = LocalStorage.getPassword(context)
         return !email.isNullOrBlank() && !password.isNullOrBlank()
+    }
+
+    fun getUsername(context: Context): String? {
+        val prefs: SharedPreferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+        return prefs.getString(KEY_USERNAME, null)
     }
 
 
