2af6d62f0c978d88de17fa7d70cd7d2456d6312a
Binukalewke <binukalewke8@gmail.com>
2025-05-03 11:17:04 +0530

    object created to load movie data from the JSON file on GitHub safely

---
+++ app/src/main/java/com/example/movienew/data/GithubJson.kt
@@ -0,0 +1,34 @@
+package com.example.movienew.data
+
+import com.example.movienew.model.Movie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.json.JSONArray
+import java.net.URL
+
+object GitHubJsonFetcher {
+    suspend fun fetchMoviesFromGitHub(): List<Movie> = withContext(Dispatchers.IO) {
+        val jsonUrl =
+            "https://raw.githubusercontent.com/Binukalewke/Dream_Land_Json/refs/heads/main/movies.json"
+        val response = URL(jsonUrl).readText() // still runs on IO thread safely
+
+        val movieList = mutableListOf<Movie>()
+        val jsonArray = JSONArray(response)
+
+        for (i in 0 until jsonArray.length()) {
+            val obj = jsonArray.getJSONObject(i)
+            val movie = Movie(
+                title = obj.getString("title"),
+                description = obj.getString("description"),
+                rating = obj.getDouble("rating"),
+                posterName = obj.getString("posterName"),
+                category = obj.optString("category", ""),
+                type = obj.optString("type", "")
+            )
+
+            movieList.add(movie)
+        }
+
+        return@withContext movieList
+    }
+}
